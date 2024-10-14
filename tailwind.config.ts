7c0da7046cf9b21cf29ff5051e2f26d3cda17dfa
mithun.s <mithun.s@qed42.com>
2024-10-14 13:50:33 +0530

    LAAHA-001: Initial Next Drupal setup for laaha.

---
+++ tailwind.config.ts
@@ -0,0 +1,17 @@
+import type { Config } from "tailwindcss"
+
+const config: Config = {
+  content: [
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/typography")],
+}
+
+export default config
